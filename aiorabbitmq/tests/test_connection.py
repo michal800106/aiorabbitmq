9693b1dddd6006b34b99874f00d82c61f8b5a8f9
Jared Mackey <jmackey@perfectpitchtech.com>
2016-04-04 10:04:55 -0600

    Changed tests over to finding connection through api using uuid instead of less reliable counting connections.

--- aiorabbitmq/tests/test_connection.py
+++ aiorabbitmq/tests/test_connection.py
@@ -5,6 +5,14 @@ from aiorabbitmq.tests import testcase
 
 
 class ConnectionTestCase(testcase.RabbitTestCase, unittest.TestCase):
+    async def find_conn(self, conn):
+        rabbit_conn = None
+        connections = self.http_client.get_connections()
+        for c in connections:
+            if c['client_properties']['uuid'] == str(conn.uuid):
+                rabbit_conn = c
+                break
+        return rabbit_conn
 
     @testcase.coroutine
     async def test_connection_setup(self):
@@ -12,29 +20,29 @@ class ConnectionTestCase(testcase.RabbitTestCase, unittest.TestCase):
         self.assertFalse(conn.connected)
         await conn.connect()
         self.assertTrue(conn.connected)
-        self.assertEqual(len(self.http_client.get_connections()), 1)
+        self.assertIsNotNone(await self.find_conn(conn))
         chan = await conn.channel()
         self.assertTrue(chan.is_open)
         await conn.close()
         # Driver has issue closing connection, should be fixed soon.
-        # self.assertEqual(len(self.http_client.get_connections()), 0)
+        # self.assertIsNone(await self.find_conn(conn))
 
     @testcase.coroutine
     async def test_connection_teardown(self):
         conn = connection(*self.connection_args)
         await conn.connect()
         self.assertTrue(conn.connected)
-        self.assertEqual(len(self.http_client.get_connections()), 1)
+        self.assertIsNotNone(await self.find_conn(conn))
         await conn.close()
         self.assertFalse(conn.connected)
         # Driver has issue closing connection, should be fixed soon.
-        # self.assertEqual(len(self.http_client.get_connections()), 0)
+        # self.assertIsNone(await self.find_conn(conn))
 
     @testcase.coroutine
     async def test_connection_manager(self):
         async with connection(*self.connection_args) as conn:
             self.assertTrue(conn.connected)
-            self.assertEqual(len(self.http_client.get_connections()), 1)
+            self.assertIsNotNone(await self.find_conn(conn))
         # Driver has issue closing connection, should be fixed soon.
-        # self.assertEqual(len(self.http_client.get_connections()), 0)
+        # self.assertIsNone(await self.find_conn(conn))
 
