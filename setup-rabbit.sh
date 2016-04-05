89e4d3842e5bd0d4cca31f486c48ec08d471fb50
Jared Mackey <jmackey@perfectpitchtech.com>
2016-04-04 19:37:06 -0600

    travis tweaks

--- setup-rabbit.sh
+++ setup-rabbit.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-${RABBITMQCTL:="sudo rabbitmqctl"}
-${RABBITMQ_PLUGINS:="sudo rabbitmq-plugins"}
 
-$RABBITMQ_PLUGINS enable rabbitmq_management
-$RABBITMQ_PLUGINS enable rabbitmq_federation
-$RABBITMQ_PLUGINS enable rabbitmq_federation_management
-sudo RABBITMQ_NODE_PORT=5673 RABBITMQ_SERVER_START_ARGS="-rabbitmq_management listener [{port,15673}]" RABBITMQ_NODENAME=hare rabbitmq-server -detached
-sudo service rabbitmq-server restart
 
-$RABBITMQCTL add_vhost /
-$RABBITMQCTL add_user guest guest
-$RABBITMQCTL set_permissions -p / guest ".*" ".*" ".*"
-$RABBITMQCTL add_vhost "aiorabbit"
-$RABBITMQCTL add_user guest guest
-$RABBITMQCTL set)permissions -p aiorabbit guest ".*" ".*" ".*"
-
+#${RABBITMQCTL:="sudo rabbitmqctl"}
+#${RABBITMQ_PLUGINS:="sudo rabbitmq-plugins"}
+#
+#$RABBITMQ_PLUGINS enable rabbitmq_management
+#$RABBITMQ_PLUGINS enable rabbitmq_federation
+#$RABBITMQ_PLUGINS enable rabbitmq_federation_management
+#sudo RABBITMQ_NODE_PORT=5673 RABBITMQ_SERVER_START_ARGS="-rabbitmq_management listener [{port,15673}]" RABBITMQ_NODENAME=hare rabbitmq-server -detached
+#sudo service rabbitmq-server restart
+#
+#$RABBITMQCTL add_vhost /
+#$RABBITMQCTL add_user guest guest
+#$RABBITMQCTL set_permissions -p / guest ".*" ".*" ".*"
+#$RABBITMQCTL add_vhost "aiorabbit"
+#$RABBITMQCTL add_user guest guest
+#$RABBITMQCTL set)permissions -p aiorabbit guest ".*" ".*" ".*"
+#
